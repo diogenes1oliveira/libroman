d5bf15172534154e988482f7cff624e0f05a499f
Diogenes Oliveira <diogenes1oliveira+github@gmail.com>
2017-04-22 10:42:57 -0300

    Adding initial part of the explanation about Roman numerals

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # libroman
 A library to convert Roman numerals to their integer values.
+
+The Roman numerical system is composed by assigning numerical values to
+certain characters, as in the table below:
+
+| Character | Value |
+|-----------|-------|
+|     I     |    1  |
+|     V     |    5  |
+|     X     |   10  |
+|     L     |   50  |
+|     C     |  100  |
+|     D     |  500  |
+|     M     | 1000  |
--- include/roman.h
+++ include/roman.h
@@ -1,8 +1,16 @@
 #ifndef ROMAN_H_
 #define ROMAN_H_
 
-/* This function returns the integer value of the roman characters string
- * in input, returning -1 if it is invalid.
+/**
+ * Returns the integer value of the roman characters string.
+ * 
+ * This function parses the input string, which must correspond to a valid
+ * roman character string, which must satisfy the following conditions:
+ * 
+ * + It must be non-null and non-empty.
+ * + The characters V, L or D cannot be repeated.
+ * + The characters I, X, C or M can be repeated up to three times.
+ * + If the characters are in non-decreasing order
 */
 int roman_to_int(const char *input);
 
